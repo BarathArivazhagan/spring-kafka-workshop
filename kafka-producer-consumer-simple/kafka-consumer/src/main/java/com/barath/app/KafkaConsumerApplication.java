ce42db53b8510047f5bef2884d1a9902913c8838
Barath <barathmacdec91@gmail.com>
2017-09-30 09:49:43 +0530

    code commit

--- kafka-producer-consumer-simple/kafka-consumer/src/main/java/com/barath/app/KafkaConsumerApplication.java
+++ kafka-producer-consumer-simple/kafka-consumer/src/main/java/com/barath/app/KafkaConsumerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+
 public class KafkaConsumerApplication {
 
 	public static void main(String[] args) {

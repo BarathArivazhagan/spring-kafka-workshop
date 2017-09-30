4ea087228cd56becc9b1b365ff4fad39201dc8c0
Barath <barathmacdec91@gmail.com>
2017-09-30 09:54:10 +0530

    code commit for producer -consumer -with-partitions

---
+++ kafka-producer-with-multiple-consumer-partitions/kafka-consumer-3/kafka-consumer-3/src/main/java/com/barath/app/KafkaConsumer3Application.java
@@ -0,0 +1,12 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumer3Application.class, args);
+	}
+}
